0a8a26c11018c5c57ace87bfe4234063564a4fb9
LisaGehrt <lgehrt90@gmail.com>
2023-07-26 16:11:47 -0400

    first commit for node tutuorial

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// Modules
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative')
+require('./7-mindGrenade.js')
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
